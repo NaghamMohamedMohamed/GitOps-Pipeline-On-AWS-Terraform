46682d57ce5e9bf08f4d0e9323636eb53b7880a3
Nagham Mohamed <107771328+NaghamMohamedMohamed@users.noreply.github.com>
2025-06-09 00:12:36 +0300

    chore: Delete duplicated directories

--- Terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "gp-terraform-bucket"
-    key            = "GP-terraform-backend/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
-}
--- Terraform/modules/eks/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "private_subnet_ids" {
-  type = list(string)
-}
-
-variable "project_name" {
-  type = string
-}
